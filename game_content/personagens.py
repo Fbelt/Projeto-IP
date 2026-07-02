7c6e1ba3a1ffc7d23a4a61d7200f3091fe1f6ca0
clecio <chmm@cin.ufpe.br>
2026-07-02 17:13:14 -0300

    fix: update enemy speed initialization to 0

--- game_content/personagens.py
+++ game_content/personagens.py
@@ -96,11 +96,7 @@ class inimigo:
    def __init__(self, x, y):
         self.x = x
         self.y = y
-<<<<<<< HEAD
         self.velocidade = 0      
-=======
-        self.velocidade = 2.5
->>>>>>> a92039abc8708b97df9d850e92a291153952a600
         self.largura = 25
         self.altura = 25
         self.cor = (255, 0, 0)
